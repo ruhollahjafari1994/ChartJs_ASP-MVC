a8632ce18712172be7cedfef93eec528920f47e5
ruhollah_jafari <jruhollah@gmail.com>
2020-10-24 01:18:10 +0330

    Add project files.

---
+++ ChartJs/Models/ChartOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChartJs.Models
+{
+    public class ChartOutput
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
